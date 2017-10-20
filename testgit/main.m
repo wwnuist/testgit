27597d38573750716be626d7c007d5e683709912
lijian <lijian@lijiantekimacbook-air.local>
2017-10-20 16:12:42 +0800

    Initial Commit

---
+++ testgit/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  testgit
+//
+//  Created by lijian on 2017/10/20.
+//  Copyright © 2017年 ww. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
